8d832f96b445eda880c251ef2c552db24a6a11d4
Lucifer <luciferxhnh@gmail.com>
2025-05-13 18:01:10 +0700

    attached cv client

--- src/utils/api-domain.js
+++ src/utils/api-domain.js
@@ -1,3 +1,3 @@
-// export const DOMAIN ="http://localhost:8386"
-export const DOMAIN ="https://utem-api.vercel.app"
+export const DOMAIN ="http://localhost:8386"
+// export const DOMAIN ="https://utem-api.vercel.app"
 // export const DOMAIN ="https://api-utemploy.onrender.com"
